05faa73ec950eaef1491b95616f2fe02a2102ebf
daneydag <dgonzalez9413@richmondhillhs.org>
2022-01-20 18:30:56 +0000

    streets-lab-1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,61 @@ function App() {
       <Navbar/>
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
 
-      <DataView/>
+       <DataView
+  borough="Staten Island"
+  renterCost="$14,292"
+  ownerCost="$29,752"
+  renterIncome="$37,882"
+  ownerIncome="$94,177" 
+  rentRatio = "36%"  
+  ownerRatio = "31%"
+  /> 
+
+<DataView
+  borough="Queens"
+  renterCost="$16,812"
+  ownerCost="$29,256"
+  renterIncome="$46,549"
+  ownerIncome="$80,144"  
+  rentRatio = "36%"  
+  ownerRatio = "37%"
+  />  
+
+<DataView
+  borough="Manhattan"
+  renterCost="$18,900"
+  ownerCost="$36,252"
+  renterIncome="$62,173"
+  ownerIncome="$142,046"
+  rentRatio = "30%"  
+  ownerRatio = "26%"/>  
+
+<DataView
+  borough="Bronx"
+  renterCost="$13,176"
+  ownerCost="$28,548"
+  renterIncome="$29,302"
+  ownerIncome="$76,454"  
+  rentRatio = "45%"  
+  ownerRatio = "37%"
+  /> 
+
+
+<DataView
+  borough="Brooklyn"
+  renterCost="$15,1441"
+  ownerCost="$31,908"
+  renterIncome="$40,128"
+  ownerIncome="$83,737" 
+  rentRatio = "38%"  
+  ownerRatio = "38%"/>
+
+
+
+
+
+
+
     </div>
   );
 }
--- src/components/DataView.js
+++ src/components/DataView.js
@@ -13,11 +13,19 @@ function DataView(props) {
         <div className="data-view">
           <div className="data-view-column left right">
             <div className="text">
-              <h3 className="text-header">Renters</h3>
+              <h3 className="text-header">Renter</h3>
             </div>
             <div className="text">
               <h2 className="text-data">{props.renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
+            </div> 
+            <div className="text">
+              <h2 className="text-data">{props.renterIncome}</h2>
+              <h3 className="text-label">Annual Income</h3>
+            </div> 
+            <div className="text">
+              <h2 className="text-data">{props.rentRatio}</h2>
+              <h3 className="text-label">Cost-To-Income Ratio</h3>
             </div>
           </div>
           <div className="data-view-column right">
@@ -25,8 +33,16 @@ function DataView(props) {
               <h3 className="text-header">Homeowners</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">{}</h2>
-              <h3 className="text-label">Annual Housing Cost</h3>
+              <h2 className="text-data">{props.ownerCost}</h2>
+              <h3 className="text-label">Annual Housing Cost</h3> 
+             <div className="text">
+              <h2 className="text-data">{props.ownerIncome}</h2>
+              <h3 className="text-label"> Annual Income</h3> 
+              <div className="text">
+              <h2 className="text-data">{props.ownerRatio}</h2>
+              <h3 className="text-label">Cost-To-Income Ratio</h3>
+            </div>
+            </div>
             </div>
           </div>
         </div>
